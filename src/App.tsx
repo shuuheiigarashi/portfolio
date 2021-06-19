aad12b1ca3cf5d2c468cecb2971304de411525f2
shuuheiigarashi <shuuhei.igarashi0618@gmail.com>
2021-06-19 12:12:51 +0900

    fix

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './components/router/Router';
+import theme  from './theme/theme';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+        <BrowserRouter>
+        <Router />
+      </BrowserRouter>      
+    </ChakraProvider>
+  );
+}
+
+export default App;
