a44fc0cc1f5442075aed0f30ee13f6451a97a073
Shuuhei Igarashi <shuuheiigarashi@USERnoMacBook-Pro.local>
2024-03-15 17:27:18 +0900

    情報を更新する

--- src/components/organisms/work/SkillCard.tsx
+++ src/components/organisms/work/SkillCard.tsx
@@ -1,33 +1,36 @@
 import { memo, VFC } from "react";
 import { Box, Image, Stack, Text } from "@chakra-ui/react";
 
-
 type Props = {
-    title: string;
-    contents: string;
-}
+  title: string;
+  contents: string;
+};
 
 export const SkillCard: VFC<Props> = memo((props) => {
-        const { title, contents } = props;
-    return (
-                <Box
-                    w="200px"
-                    h="260px"
-                    bg="white"
-                    borderRadius="10px"
-            shadow="md"
-            mx="auto"
-            p={4}
-                >
-                    <Stack textAlign="center">
-                        <Image
-                            boxSize="160px"
-                            m="auto"
-                            src="https://source.unsplash.com/random" 
-                            />
-                <Text fontSize="lg" fontWeight="bold">{title}</Text>
-                <Text fontSize="sm" color="gray">{contents}</Text>
-                    </Stack>
-                </Box>
-    )
-})
+  const { title, contents } = props;
+  return (
+    <Box
+      w="200px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      mx="auto"
+      p={4}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          m="auto"
+          src="https://source.unsplash.com/random"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {title}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {contents}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/Work.tsx
+++ src/components/pages/Work.tsx
@@ -1,24 +1,47 @@
-import { Box , Container, Flex, Wrap } from "@chakra-ui/react";
+import { Box, Container, Flex, Wrap } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { WorkCard } from "../organisms/work/WorkCard";
 
 export const Work: VFC = memo(() => {
-    return (
-        <Container textAlign="center">
-            <Box
-                as="h1"
-                color="teal.300"
-                fontSize="50px"
-            >
-            Work
-        </Box>
-            {/* <Box >Coming soon...</Box> */}
-            <Flex flexWrap="wrap" justifyContent="center">
-                            <Wrap p={{ base: 4, md: 10 }}>
-            <WorkCard />
-                    </Wrap>
-            </Flex>
-
-        </Container>
-    )
-})
+  const onNextClick = () => {
+    window.open("https://github.com/shuuheiigarashi/next-graphal", "_blank");
+  };
+  const onNestClick = () => {
+    window.open(
+      "https://github.com/shuuheiigarashi/nest-introduction",
+      "_blank"
+    );
+  };
+  const onGoClick = () => {
+    window.open("https://github.com/shuuheiigarashi/go-rest-api", "_blank");
+  };
+  return (
+    <Container textAlign="center">
+      <Box as="h1" color="teal.300" fontSize="50px">
+        Work
+      </Box>
+      <Flex flexWrap="wrap" justifyContent="center">
+        <Wrap p={{ base: 4, md: 10 }}>
+          <WorkCard
+            onClick={onNextClick}
+            title="Next"
+            imageUrl="./nextjs-icon.svg"
+            contents="Next.jsとGraphQLを使ってページを作成しております。"
+          />
+          <WorkCard
+            onClick={onNestClick}
+            title="Nest"
+            imageUrl="./nestjs-icon.svg"
+            contents="NestJSとGraphQLを使ってAPIを作成しております。"
+          />
+          <WorkCard
+            onClick={onGoClick}
+            title="Go"
+            imageUrl="./Go-Logo_Blue.png"
+            contents="GOを使ってAPIを作成しております。"
+          />
+        </Wrap>
+      </Flex>
+    </Container>
+  );
+});
